704149661a12ce2cbec89ab8cc870243d45f2df0
Vikram <vikram.sh86@gmail.com>
2024-08-21 13:37:22 +0200

    Added favorite Screen and TabBar

--- App.tsx
+++ App.tsx
@@ -5,20 +5,49 @@ import SearchScreen from './src/screens/SearchScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import MovieDetailScreen from './src/screens/MovieDetailScreen';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FavoriteScreen from './src/screens/FavoriteScreen';
+import { AntDesign } from '@expo/vector-icons';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+/// Define the Main Tab Navigator with only Home and Favorites
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = "home";
+          } else if (route.name === "Favorites") {
+            iconName = "heart";
+          }
+
+          return <AntDesign name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    
+    >
+      <Tab.Screen name="Home" component={HomeScreen} options={{headerShown:false}}/>
+      <Tab.Screen name="Favorites" component={FavoriteScreen} options={{headerShown: false}} />
+    </Tab.Navigator>
+  );
+}
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Popular" component={HomeScreen}/>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="MainTabs" component={MainTabs} />
         <Stack.Screen name="Search" component={SearchScreen} />
         <Stack.Screen name="Detail" component={MovieDetailScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
-
- // return <HomeScreen/>;
 }
 
---
+++ src/screens/FavoriteScreen.tsx
@@ -0,0 +1,107 @@
+import { View, Text, StyleSheet, TouchableOpacity, Image, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Movie } from '../viewModels/MovieViewModel'
+import { FavoritesViewModel } from '../viewModels/FavoritesViewModel';
+import { useNavigation } from '@react-navigation/native';
+
+const FavoriteScreen = () => {
+    const [favorites, setFavorites] = useState<Movie[]>([]);
+    const favoritesViewModel = new FavoritesViewModel();
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        const fetchFavorites = async () => {
+            const favoriteMovies = await favoritesViewModel.getFavoritesMovies()
+            setFavorites(favoriteMovies);
+        };
+
+        fetchFavorites();
+    },[]);
+
+
+    const renderFavoriteItem = ({item}: {item: Movie}) => (
+        <TouchableOpacity onPress={ navigation.navigate('Detail', {movie: item})} >
+            <View style = {styles.container}>
+                <Image
+                    source={ {uri: `https://image.tmdb.org/t/p/w200${item.poster_path}` }} 
+                    style={styles.poster}
+                />
+                <View style={styles.info}>
+                    <Text style={styles.title} numberOfLines={2} ellipsizeMode="tail">{item.title}</Text>
+                    <Text style={styles.releaseDate}>{item.release_date}</Text>
+                </View>
+            </View>
+        </TouchableOpacity>
+    );
+
+  return (
+    <View style={styles.container}>
+        <Text style={styles.heading}>Your Favorite Movies</Text>
+        <FlatList
+            data={favorites}
+            renderItem={renderFavoriteItem}
+            keyExtractor={(item) => item.id.toString()}
+            ListEmptyComponent={<Text style={styles.emptyText}>No favorites added yet</Text>}
+        />
+    </View>
+  )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 10,
+        backgroundColor: '#f0f0f0',
+    },
+
+    heading: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+
+    movieItem: {
+        flexDirection: 'row',
+        padding: 10,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        marginBottom: 10,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 3
+    },
+
+    poster: {
+        width: 80,
+        height: 120,
+        borderRadius: 8
+    },
+
+    info: {
+        marginLeft: 10,
+        justifyContent: 'center',
+        flex: 1
+    },
+
+    title: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+
+    releaseDate: {
+        fontSize: 14,
+        color: 'gray',
+        marginTop: 5
+    },
+
+    emptyText: {
+        textAlign: 'center',
+        marginTop: 50,
+        fontSize: 18,
+        color: 'gray',
+    }
+})
+
+export default FavoriteScreen
